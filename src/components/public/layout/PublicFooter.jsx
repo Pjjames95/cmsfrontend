f3e820234a5c51ea82ca28ae49caeabbad903758
Pjjames95 <gachombajames7@gmail.com>
2026-03-21 16:25:43 +0300

    church management system configured with supabase for demo purposes and sample data and images

--- src/components/public/layout/PublicFooter.jsx
+++ src/components/public/layout/PublicFooter.jsx
@@ -68,6 +68,7 @@ const PublicFooter = () => {
         {/* Copyright */}
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
           <p>&copy; {currentYear} Grace Church. All rights reserved.</p>
+          <p>Designed and developed by Gtech Labs</p>
         </div>
       </div>
     </footer>
